7ba0edbb1496c5d1bedabc581c6434531b2cabe1
Marcel Barbosa Bispo dos Santos <marcelbbs@gmail.com>
2017-08-19 00:24:24 -0300

    alteracao
    navegacao

--- js/index/navega-mes.js
+++ js/index/navega-mes.js
@@ -20,3 +20,27 @@ function getDescricaoProximoMesAno(mesAno){
     var descricaoProxMes = getDescricaoMes(indiceProximoMes);
     return descricaoProxMes + " / " + proximoAno;
 }
+
+var btmAntMes = document.querySelector("#mes-anterior");
+
+btmAntMes.addEventListener("click", function(){
+    var spanMesAno = document.querySelector("#mesAno");
+    var mesAno = spanMesAno.textContent;
+    var anteriorMesAno = getDescricaoAnteriorMesAno(mesAno);
+    spanMesAno.textContent = anteriorMesAno;
+})
+
+function getDescricaoAnteriorMesAno(mesAno){
+    var arrayMesAno = mesAno.split("/");
+    var mes = arrayMesAno[0].trim();
+    var ano  = arrayMesAno[1].trim();
+    var indiceMesAtual = descobreIndiceMes(mes);
+    var indiceAnteriorMes = indiceMesAtual -1;
+    var anteriorAno = parseInt(ano);
+    if(indiceAnteriorMes==-1){
+        anteriorAno -=1;
+        indiceAnteriorMes=11;
+    }
+    var descricaoAntMes = getDescricaoMes(indiceAnteriorMes);
+    return descricaoAntMes +" / "+anteriorAno;
+}
